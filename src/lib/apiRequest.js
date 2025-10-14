4adec14e8761e94b7a042d562951103a94c3863e
myavuz <muhametyavuz@hotmail.com>
2025-10-14 16:17:33 +0300

    fix

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -3,6 +3,10 @@ import axios from "axios";
 const apiRequest = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  credentials: "include",
 });
 
 export default apiRequest;
