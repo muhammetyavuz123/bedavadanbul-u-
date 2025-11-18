ca7cec31bad96644f54b3d4dc98484c4c70734b4
myavuz <muhametyavuz@hotmail.com>
2025-11-18 12:00:42 +0300

    slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,26 +1,32 @@
 import { useState } from "react";
 import "./slider.scss";
+import defaulImage from "../../assets/r.png";
 
-function Slider({ images }) {
+function Slider({ images = [] }) {
   const [active, setActive] = useState(0);
   const [fullscreen, setFullscreen] = useState(false);
 
+  // Eğer images boşsa fallback olarak default image kullan
+  const sliderImages = images.length > 0 ? images : [defaulImage];
+
+  const getImage = (img) => (img ? img : defaulImage);
+
   const prevSlide = () =>
-    setActive(active === 0 ? images.length - 1 : active - 1);
+    setActive(active === 0 ? sliderImages.length - 1 : active - 1);
   const nextSlide = () =>
-    setActive(active === images.length - 1 ? 0 : active + 1);
+    setActive(active === sliderImages.length - 1 ? 0 : active + 1);
 
   return (
     <div className="slider">
       <div className="mainSlide" onClick={() => setFullscreen(true)}>
-        <img src={images[active]} alt={`slide-${active}`} />
+        <img src={getImage(sliderImages[active])} alt={`slide-${active}`} />
       </div>
 
       <div className="thumbs">
-        {images.map((img, idx) => (
+        {sliderImages.map((img, idx) => (
           <img
             key={idx}
-            src={img}
+            src={getImage(img)}
             className={active === idx ? "active" : ""}
             onClick={() => setActive(idx)}
             alt={`thumb-${idx}`}
@@ -34,7 +40,7 @@ function Slider({ images }) {
             <img src="/arrow.png" alt="prev" />
           </div>
           <div className="fullImage">
-            <img src={images[active]} alt={`full-${active}`} />
+            <img src={getImage(sliderImages[active])} alt={`full-${active}`} />
           </div>
           <div className="arrow right" onClick={nextSlide}>
             <img src="/arrow.png" alt="next" className="right" />
